de7614599b2098c978e2434d9788c5f47822c134
Ardian123 <suzdorfa@gmail.com>
2016-03-15 14:46:04 +0100

    Commit Message

--- app/src/main/java/com/example/kuznargroup/welowanie/MenuActivity.java
+++ app/src/main/java/com/example/kuznargroup/welowanie/MenuActivity.java
@@ -11,7 +11,7 @@ import com.example.ardian.menuexample.R;
 /**
  * Created by Ardian on 2016-03-14.
  */
-public class MenuActivity extends AppCompatActivity {
+public class git initMenuActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
