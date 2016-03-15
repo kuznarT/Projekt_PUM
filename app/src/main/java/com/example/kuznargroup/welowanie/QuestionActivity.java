6069e28dfecdc1c6fd36f89f2c0e61cfb3fc619b
Ardian123 <suzdorfa@gmail.com>
2016-03-15 14:23:49 +0100

    1.03

--- app/src/main/java/com/example/kuznargroup/welowanie/QuestionActivity.java
+++ app/src/main/java/com/example/kuznargroup/welowanie/QuestionActivity.java
@@ -1,20 +1,38 @@
 package com.example.kuznargroup.welowanie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.ardian.menuexample.R;
 
 public class QuestionActivity extends AppCompatActivity {
 
+    int position = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
+
+        //Wybór poziomu od 0 do 7, czyli od Semestru I do poziomu Bonusowego
+        Intent intent = getIntent();
+        position = intent.getExtras().getInt("Position");
+
+        //Tutaj przydałby się serwer
+        if (position == 0){
+            Toast.makeText(QuestionActivity.this, "Cos", Toast.LENGTH_SHORT).show();
+        }
+        else if (position == 1){
+            Toast.makeText(QuestionActivity.this, "Cos", Toast.LENGTH_SHORT).show();
+        }
+
+
 
 
     }
--- app/src/main/java/com/example/kuznargroup/welowanie/SelectStageActivity.java
+++ app/src/main/java/com/example/kuznargroup/welowanie/SelectStageActivity.java
@@ -1,40 +1,42 @@
 package com.example.kuznargroup.welowanie;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.ardian.menuexample.R;
 
 
-/**
- * Created by Ardian on 2016-03-14.
- */
 public class SelectStageActivity extends AppCompatActivity {
 
     ArrayAdapter<String> adapter;
     ListView listView;
-    String[] stages= {"Semestr I", "Semestr II", "Semestr III", "Semestr IV", "Semestr V", "Semestr VI", "Bonus Best Of" };
+    String[] stages;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_stage);
 
+        stages = getResources().getStringArray(R.array.level_difficulty);
+
         listView = (ListView) findViewById(R.id.stagesListView);
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,stages);
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, stages);
         listView.setAdapter(adapter);
-//        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                //tutaj dalszy kod
-//            }
-//        });
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
+                Intent intent = new Intent (getApplicationContext(), QuestionActivity.class);
+                intent.putExtra("Position", position);
+                startActivity(intent);
+            }
+        });
 
 
     }
