f3b73d4ceb7c946c2d8a169248c41cb702f5fedb
Ardian123 <suzdorfa@gmail.com>
2016-04-04 18:53:00 +0200

    Commit message

--- app/src/main/java/com/example/kuznargroup/welowanie/EndGameActivity.java
+++ app/src/main/java/com/example/kuznargroup/welowanie/EndGameActivity.java
@@ -1,13 +1,27 @@
 package com.example.kuznargroup.welowanie;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class EndGameActivity extends AppCompatActivity {
+public class EndGameActivity extends Activity {
+
+    Button buttonOnReturnToSelectStageClick;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_end_game);
+
+        buttonOnReturnToSelectStageClick.setBackgroundResource(R.drawable.button_menu);
+    }
+
+    public void onReturnToSelectStageClick(View view) {
+        buttonOnReturnToSelectStageClick.setBackgroundResource(R.drawable.button_menu_click);
+        Intent intent = new Intent(this, SelectStageActivity.class);
+        startActivity(intent);
+
     }
 }
