ebc154e59ea9437400931bc5771764e09c134235
Ardian123 <suzdorfa@gmail.com>
2016-05-04 23:49:03 +0200

    Licznik

--- app/src/main/java/com/example/kuznargroup/welowanie/QuestionActivity.java
+++ app/src/main/java/com/example/kuznargroup/welowanie/QuestionActivity.java
@@ -3,6 +3,7 @@ package com.example.kuznargroup.welowanie;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.os.Handler;
 import android.util.Log;
 import android.view.View;
@@ -25,13 +26,15 @@ public class QuestionActivity extends Activity {
     Button buttonC;
     Button buttonD;
     TextView textView;
+    TextView count;
     Retrofit retrofit;
     QuestionService questionService;
     Call<pytania> QACall;
     pytania questionsAnswers;
-    int licz=0,punkty=0;
+    int licz = 0, punkty = 0;
     String points;
-
+    int resultTime=0, resultTime1=0, resultTime2=0,resultTime1ASCII=0,resultTime2ASCII=0;
+    CountDownTimer ct;
 
 
     @Override
@@ -44,18 +47,29 @@ public class QuestionActivity extends Activity {
         buttonC = (Button) findViewById(R.id.buttonC);
         buttonD = (Button) findViewById(R.id.buttonD);
         textView = (TextView) findViewById(R.id.textView_pytanie);
+        count = (TextView) findViewById(R.id.countText);
 
         buttonA.setBackgroundResource(R.drawable.button_game);
         buttonB.setBackgroundResource(R.drawable.button_game);
         buttonC.setBackgroundResource(R.drawable.button_game);
         buttonD.setBackgroundResource(R.drawable.button_game);
 
+        ct =  new CountDownTimer(60000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
 
 
-        //Wybór poziomu od 0 do 7, czyli od Semestru I do poziomu Bonusowego
-       // Intent intent = getIntent();
-       // position = intent.getExtras().getInt("Position");
-       // position++; //Impossible to use this as id if it gets value 0 (this is simple but stupid solution)
+                String v = String.format("%02d", millisUntilFinished/60000);
+                int va = (int)( (millisUntilFinished%60000)/1000);
+                count.setText(String.format("%02d", va));
+            }
+
+            public void onFinish() {
+                count.setText("0");
+            }
+        };
+        ct.start();
+
 
         //Retrofit magic part
         retrofit = new Retrofit.Builder()
@@ -70,26 +84,22 @@ public class QuestionActivity extends Activity {
         QACall.enqueue(new Callback<pytania>() {
             @Override
             public void onResponse(Call<pytania> call, Response<pytania> response) {
-                if(response.isSuccessful()) {
+                if (response.isSuccessful()) {
                     questionsAnswers = response.body();
 
-                   // textView.setText(Integer.toString(questionsAnswers.success));
+                    // textView.setText(Integer.toString(questionsAnswers.success));
                     giveQuestions();
 
 
-
-                    for(Questions c : questionsAnswers.Questions) {
-                        Log.i(TAG,String.format("%s: %s",c.question,c.answer1));
-
+                    for (Questions c : questionsAnswers.Questions) {
+                        Log.i(TAG, String.format("%s: %s", c.question, c.answer1));
 
 
-                        Log.i(TAG,"---------");
+                        Log.i(TAG, "---------");
                     }
 
 
-                }
-                else
-                {
+                } else {
                     Toast.makeText(QuestionActivity.this, "LOL2", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -102,70 +112,69 @@ public class QuestionActivity extends Activity {
         });
 
 
-
     }
 
 
     public void onAnswer(View view) {
         licz++;
 
-            buttonA.setBackgroundResource(R.drawable.button_game);
-            buttonB.setBackgroundResource(R.drawable.button_game);
-            buttonC.setBackgroundResource(R.drawable.button_game);
-            buttonD.setBackgroundResource(R.drawable.button_game);
+        buttonA.setBackgroundResource(R.drawable.button_game);
+        buttonB.setBackgroundResource(R.drawable.button_game);
+        buttonC.setBackgroundResource(R.drawable.button_game);
+        buttonD.setBackgroundResource(R.drawable.button_game);
 
 
-            switch (view.getId()) {
-                case R.id.buttonA:
-                    buttonA.setBackgroundResource(R.drawable.button_game_click);
-                    if(buttonA.getText() == questionsAnswers.Questions.get(licz-1).answer1)
-                    {
-                        Toast.makeText(QuestionActivity.this,"Pan to umie ale tego nie rozumie :D",Toast.LENGTH_LONG).show();
-                        punkty++;
-                    }
-                    else{
-                        Toast.makeText(QuestionActivity.this,"...Bania.",Toast.LENGTH_LONG).show();
-                    }
-                    break;
+        switch (view.getId()) {
+            case R.id.buttonA:
+                buttonA.setBackgroundResource(R.drawable.button_game_click);
 
-                case R.id.buttonB:
-                    buttonB.setBackgroundResource(R.drawable.button_game_click);
-                    if(buttonB.getText() == questionsAnswers.Questions.get(licz-1).answer1)
-                    {
-                        Toast.makeText(QuestionActivity.this,"Pan to umie ale tego nie rozumie :D",Toast.LENGTH_LONG).show();
-                        punkty++;
-                    }
-                    else{
-                        Toast.makeText(QuestionActivity.this,".... Bania.",Toast.LENGTH_LONG).show();
-                    }
-                    break;
-                case R.id.buttonC:
-                    buttonC.setBackgroundResource(R.drawable.button_game_click);
-                    if(buttonC.getText() == questionsAnswers.Questions.get(licz-1).answer1)
-                    {
-                        Toast.makeText(QuestionActivity.this,"Pan to umie ale tego nie rozumie :D",Toast.LENGTH_LONG).show();
-                        punkty++;
-                    }
-                    else{
-                        Toast.makeText(QuestionActivity.this,".... Bania.",Toast.LENGTH_LONG).show();
-                    }
-                    break;
-                case R.id.buttonD:
-                    buttonD.setBackgroundResource(R.drawable.button_game_click);
+                if (buttonA.getText() == questionsAnswers.Questions.get(licz - 1).answer1) {
+                    Toast.makeText(QuestionActivity.this, "Pan to umie ale tego nie rozumie :D", Toast.LENGTH_SHORT).show();
+                    punkty++;
+                } else {
+                    Toast.makeText(QuestionActivity.this, "...Bania.", Toast.LENGTH_SHORT).show();
+                }
+                break;
 
-                    if(buttonD.getText() == questionsAnswers.Questions.get(licz-1).answer1)
-                        {
-                            Toast.makeText(QuestionActivity.this,"Pan to umie ale tego nie rozumie :D",Toast.LENGTH_LONG).show();
-                            punkty++;
-                        }
-                        else{
-                            Toast.makeText(QuestionActivity.this,".... Bania.",Toast.LENGTH_LONG).show();
-                        }
-                    break;
+            case R.id.buttonB:
+                buttonB.setBackgroundResource(R.drawable.button_game_click);
+                if (buttonB.getText() == questionsAnswers.Questions.get(licz - 1).answer1) {
+                    Toast.makeText(QuestionActivity.this, "Pan to umie ale tego nie rozumie :D", Toast.LENGTH_SHORT).show();
+                    punkty++;
+                } else {
+                    Toast.makeText(QuestionActivity.this, "...Bania.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.buttonC:
+                buttonC.setBackgroundResource(R.drawable.button_game_click);
+                if (buttonC.getText() == questionsAnswers.Questions.get(licz - 1).answer1) {
+                    Toast.makeText(QuestionActivity.this, "Pan to umie ale tego nie rozumie :D", Toast.LENGTH_SHORT).show();
+                    punkty++;
+                } else {
+                    Toast.makeText(QuestionActivity.this, "...Bania.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+            case R.id.buttonD:
+                buttonD.setBackgroundResource(R.drawable.button_game_click);
+
+                if (buttonD.getText() == questionsAnswers.Questions.get(licz - 1).answer1) {
+                    Toast.makeText(QuestionActivity.this, "Pan to umie ale tego nie rozumie :D", Toast.LENGTH_SHORT).show();
+                    punkty++;
+                } else {
+                    Toast.makeText(QuestionActivity.this, "...Bania.", Toast.LENGTH_SHORT).show();
+                }
+                break;
 
 
-            }
-            if(licz == 5){
+        }
+        if (licz == 5) {
+            String resultTimeString = ct.toString();
+            resultTime1ASCII = resultTimeString.charAt(resultTimeString.length() - 2);
+            resultTime2ASCII = resultTimeString.charAt(resultTimeString.length()-1);
+            resultTime1 = resultTime1ASCII - 48;
+            resultTime2 = resultTime2ASCII - 48;
+            resultTime = resultTime1 + resultTime2;
+            punkty = punkty * resultTime;
             Intent intent = new Intent(QuestionActivity.this, YourResultActivity.class);
             startActivity(intent);
 
@@ -176,26 +185,26 @@ public class QuestionActivity extends Activity {
             startActivityForResult(intent, wynik);
             finish();
 
-        }else {
-                Runnable r = new Runnable() {
-                    @Override
-                    public void run() {
-                        buttonA.setBackgroundResource(R.drawable.button_game);
-                        buttonB.setBackgroundResource(R.drawable.button_game);
-                        buttonC.setBackgroundResource(R.drawable.button_game);
-                        buttonD.setBackgroundResource(R.drawable.button_game);
-                        giveQuestions();
-                    }
-                };
+        } else {
+            Runnable r = new Runnable() {
+                @Override
+                public void run() {
+                    buttonA.setBackgroundResource(R.drawable.button_game);
+                    buttonB.setBackgroundResource(R.drawable.button_game);
+                    buttonC.setBackgroundResource(R.drawable.button_game);
+                    buttonD.setBackgroundResource(R.drawable.button_game);
+                    giveQuestions();
+                }
+            };
 
-                Handler h = new Handler();
-                h.postDelayed(r,300);
+            Handler h = new Handler();
+            h.postDelayed(r, 300);
 
-            }
+        }
 
     }
 
-    public void giveQuestions(){
+    public void giveQuestions() {
         questionsAnswers.Questions.get(licz).ShuffleAnswers();
         textView.setText(questionsAnswers.Questions.get(licz).question);
         buttonA.setText(questionsAnswers.Questions.get(licz).getAnswer(0));
@@ -203,5 +212,6 @@ public class QuestionActivity extends Activity {
         buttonC.setText(questionsAnswers.Questions.get(licz).getAnswer(2));
         buttonD.setText(questionsAnswers.Questions.get(licz).getAnswer(3));
     }
+
 
 }
